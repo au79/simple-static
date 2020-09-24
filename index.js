db942009404b28c8becd6a24394533ae7e895b2f
Christopher Goldman <dev@oolong.com>
2020-09-23 17:09:03 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+const successFile = require('./public/success.json');
+const emptyFile = require('./public/empty.json');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('An alligator approaches!');
+});
+
+app.post('/success', function (req, res) {
+    res.json(successFile);
+})
+
+app.post('/empty', function (req, res) {
+    res.json(emptyFile);
+})
+
+app.post('/error', function (req, res) {
+    res.status(500).send();
+})
+
+app.listen(3000, () => console.log('Gator app listening on port 3000!'));
